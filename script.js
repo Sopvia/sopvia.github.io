02c9224aa35e7b09297ecb4b466856b1dc44024d
Sopvia <sophiaschreck03@gmail.com>
2025-12-18 19:52:18 +0100

    design change

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ function switchLang() {
   }
 
   if (language == "german") {
-    document.getElementById("welcomeBg").textContent = "Wilkommen";
+    document.getElementById("welcomeBg").textContent = "Willkommen auf meinem Portfolio";
     welcomeText = 'Wilkommen';
     document.documentElement.style.setProperty('--welcome', '"' + welcomeText + '"');
     document.getElementById("aboutMe").textContent = "Über mich";
     document.getElementById("intro").textContent =
       "Hallo, mein Name ist Sophia. Ich mache gerade eine Ausbildung zur Fachinformatikerin. Im Moment lerne ich Python, Java und GDscript.";
-    document.getElementById("github").textContent =
-      "Klicken Sie hier für mein Github-Profil.";
     document.getElementById("skills").textContent = "Meine Fähigkeiten";
     document.getElementById("projects").textContent = "Projekte";
     document.getElementById("descSprite").textContent =
@@ -27,14 +25,12 @@ function switchLang() {
     document.getElementById("descKaplan").textContent =
       "Ein einfaches Adressbuch in Python mit customtkinter GUI.";
   } else if (language == "english") {
-    document.getElementById("welcomeBg").textContent = "Welcome";
+    document.getElementById("welcomeBg").textContent = "Welcome to my Portfolio";
     welcomeText = 'Welcome';
     document.documentElement.style.setProperty('--welcome', '"' + welcomeText + '"');
     document.getElementById("aboutMe").textContent = "About me";
     document.getElementById("intro").textContent =
       "Hi, my name is Sophia. I'm currently training to be an IT specialist. At the moment i'm learning Python, Java and GDscript.";
-    document.getElementById("github").textContent =
-      "Click here for my Github profile.";
     document.getElementById("skills").textContent = "My Skills";
     document.getElementById("projects").textContent = "Projects";
     document.getElementById("descSprite").textContent =
@@ -61,3 +57,37 @@ function closeBar() {
   document.getElementById("controlbar").style.display = "none";
   document.getElementById("openbtn").style.display = "block";
 }
+
+let slideIndex = 1;
+
+window.addEventListener('load', () => {
+  showSlides(slideIndex);
+})
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+} 
